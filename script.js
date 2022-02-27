415d0cd9331d03ccf54a94f88d4c275a5cc92448
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-02-27 16:55:41 +0300

    add styles

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function hamburger_menu() {
-  var x = document.querySelector(".menu_links");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+
+function toggleMobileMenu(menu) {
+  menu.classList.toggle('open');
 }
-hamburger_menu()
