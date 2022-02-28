5584c0bb7269ffcb41e03575d0b97eede9cfd9d3
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-02-28 19:37:00 +0300

    add styles

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
-function toggleMobileMenu(menu) {
-  menu.classList.toggle('open');
-}
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+const triangular_Border=document.querySelector('.triangle_border');
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+  triangular_Border.classList.toggle('hide');
+  
+});
